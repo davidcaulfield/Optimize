e0a4976e71788e6133aa582261e2e4eba0224e0c
David <dcaulfie@stetson.edu>
2017-04-11 12:59:02 -0400

    made html changes

---
+++ static/js/ispy.js
@@ -0,0 +1,54 @@
+// $(document).ready(function() {
+
+// 	var sBrowser, sUsrAg = navigator.userAgent;
+
+
+// 	if(sUsrAg.indexOf("Chrome") -> -1) {
+// 		sBrowser = "Google Chrome";
+// 		$('#thing_one').append('
+	    
+// 	     &lt;h3 class=&quot;build_portfolio_list&quot;&gt;Objective&lt;/h3&gt;
+// 		&lt;input list=&quot;objective-list&quot; name=&quot;objective&quot; placeholder=&quot;Choose from the dropdown menu&quot; required&gt;
+// 	    &lt;datalist id=&quot;objective-list&quot;&gt;
+// 	    	&lt;option value=&quot;Growth&quot;&gt;
+// 	    	&lt;option value=&quot;Income&quot;&gt;
+// 	    	&lt;option value=&quot;Speculation&quot;&gt;
+// 		&lt;/datalist&gt;
+
+// 		&lt;h3 class=&quot;build_portfolio_list&quot;&gt;Time Horizon&lt;/h3&gt;
+// 		&lt;input list=&quot;time-horizon&quot; name=&quot;time&quot; placeholder=&quot;Choose from the dropdown menu&quot; required&gt;
+// 			&lt;datalist id=&quot;time-horizon&quot;&gt;
+// 				&lt;option value=&quot;1-5 years&quot;&gt;
+// 		    	&lt;option value=&quot;5-10 years&quot;&gt;
+// 		    	&lt;option value=&quot;10+ years&quot;&gt;
+// 		    &lt;/datalist&gt;
+// 		')
+// 	};
+// 	else if (sUsrAg.indexOf("Safari") > -1) {
+// 		sBrowser = "Apple Safari";
+// 		$('#thing_one').append('
+	   
+// 	    &lt;datalist id=&quot;objective-list&quot;&gt;
+//   			&lt;select&gt;
+// 	    	&lt;option value=&quot;Growth&quot;&gt;Growth&lt;/option&gt;
+// 	    	&lt;option value=&quot;Income&quot;&gt; Income&lt;/option&gt;
+// 	    	&lt;option value=&quot;Speculation&quot;&gt; Speculation&lt;/option&gt;
+// 	    	&lt;/select&gt;
+// 		&lt;/datalist&gt;
+// 		&lt;h3 class=&quot;build_portfolio_list&quot;&gt;Time Horizon&lt;/h3&gt;
+// 		&lt;input list=&quot;time-horizon&quot; name=&quot;time&quot; placeholder=&quot;Choose from the dropdown menu&quot; required&gt;
+// 			&lt;datalist id=&quot;time-horizon&quot;&gt;
+// 				&lt;select&gt; 
+// 				&lt;option value=&quot;1-5 years&quot;&gt;1-5 years&lt;/option&gt;
+// 		    	&lt;option value=&quot;5-10 years&quot;&gt;5-10 years&lt;/option&gt;
+// 		    	&lt;option value=&quot;10+ years&quot;&gt;10+ years&lt;/option&gt;
+// 		    	&lt;/select&gt;
+// 		    &lt;/datalist&gt;
+// 		')
+// 	};
+
+// 	console.log("You are using: " + sBrowser);
+// });
+
+
+
